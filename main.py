c13de3d2273df7841347236af5f6d35ef81af2da
gurmatsinghsour <89722217+gurmatsinghsour@users.noreply.github.com>
2023-05-06 08:37:08 +0530

    Basic pastebin with frontend

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/save")
+async def save(request: Request, value: str= Form(...)):
+    return templates.TemplateResponse("saved.html", {"request": request, "value" : value})
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=5000)
